17e926997173cf19e8fe785a712d1151c16e91b2
yoko-Developer <whitesnow.wave@gmail.com>
2024-11-08 23:16:36 +0900

    201を返すようにControllerを実装

--- src/main/java/com/example/todo_api/controller/task/TaskController.java
+++ src/main/java/com/example/todo_api/controller/task/TaskController.java
@@ -4,6 +4,7 @@ import com.example.todo_api.service.task.TaskService;
 import com.example.todoapi.controller.TasksApi;
 import com.example.todoapi.model.TaskDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController implements TasksApi {
 
     private final TaskService taskService;
+
+    @Override
+    public ResponseEntity<Void> createTask() {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
     @Override
     public ResponseEntity<TaskDTO> showTask(Long taskId) {
