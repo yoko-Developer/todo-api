ffb60607ea402965958d4609a0c901b66dae0507
yoko-Developer <whitesnow.wave@gmail.com>
2024-12-04 21:05:18 +0900

    リクエストボディを作成

--- src/main/java/com/example/todo_api/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todo_api/controller/advice/CustomExceptionHandler.java
@@ -1,16 +1,32 @@
 package com.example.todo_api.controller.advice;
 
 import com.example.todo_api.service.task.TaskEntityNotFoundException;
+import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.ResourceNotFoundError;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.springframework.http.ResponseEntity.status;
 
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        return ResponseEntity.badRequest().body(new BadRequestError());
+    }
 
     @ExceptionHandler(TaskEntityNotFoundException.class)
     public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
